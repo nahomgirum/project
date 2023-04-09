4e0630751135497e926b6fe7d0dec997a27ac97d
nahom girum <nahomgirum41@gmail.com>
2023-04-09 11:59:27 -0700

    updated

--- home_page.php
+++ home_page.php
@@ -1,3 +1,55 @@
-hhhi
-nahom
-join
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    v<script src="https://kit.fontawesome.com/156ee3c56e.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/home.css">
+    <title>home page</title>
+</head>
+<body>
+    
+
+
+<!--header start -->
+
+<header>
+
+  <i  id="menu-bar" class="fas fa-bars"></i>
+
+    <a href="home" class="title"><span>C</span>elebrity Ethiopia Tour</a>
+
+    <nav class="navbar">
+    <a href="home">Home</a>
+    <a href="Book">Book now</a>
+    <a href="package">Pacakge</a>
+    <a href="history">history</a>
+    <a href="posted story">posted stories</a>
+    <a href="login">sign in</a>
+    <a href="register">sign up</a>
+    </nav>
+
+</header>
+    
+
+<!-- h str-->
+
+<section class="home" id="home">
+
+        <div class="first">
+
+            <h3>Adventure is worthwhile</h3>
+            <p>discover new places with us , adventure awaits</p>
+            <a href="#" class="form-btn">Discover more</a>
+
+        </div>
+
+
+</section>
+
+
+<!--script started-->
+<!--<script src="js/script.js"></script>-->
+</body>
+</html>
